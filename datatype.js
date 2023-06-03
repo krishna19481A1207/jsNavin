7459e293fa87d07b5248851e2564db794b10b842
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-03 16:12:19 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,27 @@
+let num1 = 9
+let name = "krishna"
+let data = 7.8
+console.log(typeof data)
+console.log(typeof name)
+let num2 = 0xf
+console.log(num2)
+
+let num3 = 1.5e12
+console.log(num3)
+
+let num4 = 100_00_000
+console.log(num4)
+let num5 = 5/0
+
+let num6 = -5/0
+console.log(num5)
+console.log(num6)
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MAX_VALUE* 10)
+
+let num7 = 1050505050505050505050505051
+console.log(num7)
+let num8 = 1050505050505050505050505051n
+console.log(num8)
+console.log(num8+2n)
