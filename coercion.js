cb6a503654681436951099f12d25848ddd1a6902
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-03 20:59:49 +0530

    Add files via upload

---
+++ coercion.js
@@ -0,0 +1,63 @@
+let num = 6;
+console.log(num,typeof num);
+let num1 = String(6)
+console.log(num1,typeof num1 )
+
+let num2 = "123"
+console.log(num2,typeof num2 )
+
+let num3 =Number("123")
+console.log(num3,typeof num3 )
+
+
+let x
+console.log(x,typeof x )
+
+x = 8
+console.log(x,typeof x )
+
+x = 8+""
+console.log(x,typeof x )
+
+x = 8 - 2
+console.log(x,typeof x )
+x = x+""
+console.log(x,typeof x )
+
+x = x-2
+console.log(x,typeof x )
+x = !x
+console.log(x,typeof x )
+x = Boolean(7)
+console.log(x,typeof x )
+x = Boolean(-7)
+console.log(x,typeof x )
+x = Boolean(0)
+console.log(x,typeof x )
+x = Boolean(null)
+console.log(x,typeof x )
+x = Boolean(undefined)
+console.log(x,typeof x )
+x = Boolean("Krishna")
+console.log(x,typeof x )
+x = 8+""
+console.log(x,typeof x )
+x = x + 2
+console.log(x,typeof x )
+x = +x +2
+console.log(x,typeof x )
+ x = "123 Navin"
+console.log(x,typeof x)
+x = Number("123 Navin")
+console.log(x,typeof x)
+
+x = parseInt("123 Navin")
+console.log(x,typeof x)
+x = parseInt("N123 Navin")
+console.log(x,typeof x)
+x = parseInt("123 N239avin")
+console.log(x,typeof x)
+x = parseInt("123 239avin")
+console.log(x,typeof x)
+x = parseInt("123239avin")
+console.log(x,typeof x)
