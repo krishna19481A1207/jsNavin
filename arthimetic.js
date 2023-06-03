d5d7403b34764079a0354002fbc4675552c9fbcc
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-03 20:59:21 +0530

    Add files via upload

---
+++ arthimetic.js
@@ -0,0 +1,15 @@
+let num1 =4
+let num2 =2
+console.log(num1 %2)
+console.log(num2%num1)
+console.log(num2/num1)
+console.log(num2+num1)
+console.log(num2-num1)
+console.log(num2*num1)
+console.log(true + true)
+console.log(num1+=1)
+console.log(num1 = num1 + 1)
+console.log(num1++)
+
+console.log(Math.pow(2,3))
+console.log(2**3)
