5ac9096012e7d5601d8459e120a95047b91437cb
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-04 09:52:46 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,46 @@
+if(true) 
+    console.log("Krishna")
+else
+    console.log("Krishna111")
+    console.log("Krishna222")
+    console.log("Krishna333")
+
+if(5<6)
+    console.log("Krishna111")
+    console.log("Krishna222")
+    console.log("Krishna333")
+    console.log("Krishna111")
+    console.log("Krishna222")
+    console.log("Krishna333")
+if(5>9)
+    {console.log("Krishna111")
+    console.log("Krishna222")
+    console.log("Krishna333")
+    }
+else if(5<6)
+{
+    console.log("Krishna111")
+    console.log("Krishna222")
+    console.log("Krishna333")
+    console.log("Krishna111")
+    console.log("Krishna222")
+    console.log("Krishna333")
+}else
+    console.log("Krishna111")
+    console.log("Krishna222")
+    console.log("Krishna333")
+
+
+let a=0, b=1,c=5;
+
+if(a>b) console.log("a is big")
+else if(b>c) console.log("b is big")
+else
+   console.log("c is big")
+   console.log("yee")
+   console.log("Krishna111")
+   console.log("Krishna222")
+   console.log("Krishna333")
+
+console.log("bye")
+
