1effa66d9a1eb450151e5cb8c96f6145772c8800
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-04 09:53:41 +0530

    Add files via upload

---
+++ relational.js
@@ -0,0 +1,9 @@
+console.log("pen" > " book")
+console.log("pen" > "pencil")
+console.log("2" > 3)
+console.log("2" > 1)
+
+let data = 3==3
+console.log(data)
+console.log("2" === 2)
+console.log("2" == 2 )
