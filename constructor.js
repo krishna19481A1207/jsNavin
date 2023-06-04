d16c84bd4c0d96e307cc73d8d7cba0fd7fd43a81
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-04 14:39:57 +0530

    Add files via upload

---
+++ constructor.js
@@ -0,0 +1,23 @@
+function Alien(name,tech)
+{
+    this.name =name;
+    this.tech =tech;
+
+this.work = function()
+    {
+        console.log("solving bugs from 12")
+    }
+
+return 7
+}
+
+let alien1 = new Alien('Navin','js');
+let alien2 = new Alien('krishna','java');
+
+console.log(alien1)
+console.log(alien2)
+
+alien1.tech = 'blockchain'
+console.log(alien1)
+alien1.work();
+
