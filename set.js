4cb458fcd79d794d7a393563b6c6b5f39a351f3c
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-04 19:52:37 +0530

    Add files via upload

---
+++ set.js
@@ -0,0 +1,14 @@
+let nums = new Set();
+
+nums.add(3)
+nums.add(4)
+nums.add(3)
+nums.add("Navin")
+nums.add("kiran")
+nums.add("chirag")
+
+console.log(nums.has("navin"));
+console.log(nums.has("Navin"));
+console.log(nums.has(2));
+
+
