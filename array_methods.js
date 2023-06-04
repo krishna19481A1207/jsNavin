bbead19590f89c0b410b25d19acc32038f5abc47
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-04 15:38:28 +0530

    Add files via upload

--- array_methods.js
+++ array_methods.js
@@ -1,6 +1,13 @@
 let data = [5,6,7,8,9]
 
-
+data.pop()
 data.push(100,101,102)
+data.shift()
+data.unshift(2)
+data.splice(2)
+data.splice(2,2)
+data.splice(2,2,11,15,16)
+data.push(2)
+data.unshift(9,9,9,7)
 console.log(data)
 
