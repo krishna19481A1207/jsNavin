8ff08f92bec0b27d25a3f220712ccc79b25b4f84
srikrishn var aripi <97373936+krishna19481A1207@users.noreply.github.com>
2023-06-04 09:56:33 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,18 @@
+align = {
+    name:"krishna",
+    tech:"js",
+    'work exp':4,
+    "dhoom 2" :5,
+}
+input = 'name'
+input1="tech"
+console.log(align, typeof align)
+console.log(align.name)
+console.log(align.tech)
+console.log(align['name'])
+console.log(align['tech'])
+console.log(align[input1])
+console.log(align.input)
+
+
+
